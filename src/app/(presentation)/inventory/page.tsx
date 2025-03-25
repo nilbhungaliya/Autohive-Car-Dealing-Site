5288dea8dcc1c5c9738811de046d936af8261967
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-03-25 22:05:51 +0530

    add some packages and add some code

--- src/app/(presentation)/inventory/page.tsx
+++ src/app/(presentation)/inventory/page.tsx
@@ -1,7 +1,10 @@
 import { ClassifiedCard } from "@/components/inventory/classified-card";
 import { ClassifiedList } from "@/components/inventory/classified-list";
-import { AwaitedPageProps, PageProps } from "@/config/types";
+import { AwaitedPageProps, favourites, PageProps } from "@/config/types";
 import db from "@/lib/db";
+import { redis } from "@/lib/redis-store";
+import { getSourceId } from "@/lib/source-id";
+
 
 const getInventory = async (searchParams: AwaitedPageProps["searchParams"]) =>{
     return db.classified.findMany({
@@ -16,7 +19,13 @@ export default async function InventoryPage(props: PageProps) {
     const classifieds = await getInventory(searchParams);
     const count = await db.classified.count();
 
+    const sourceId = await getSourceId();
+
+    const favourites = await redis.get<favourites>(sourceId ?? "");
+
+    console.log({favourites});
+
   return <div className="grid grid-cols-1">
-    <ClassifiedList classifieds={classifieds} />
+    <ClassifiedList classifieds={classifieds} favourites={favourites? favourites.ids:[]} />
   </div>;
 }
