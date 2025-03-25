6d02db3ad598f2d58f481bcb71ff762442fb4204
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-03-25 22:02:57 +0530

    updating components

--- src/components/inventory/classified-card.tsx
+++ src/components/inventory/classified-card.tsx
@@ -1,5 +1,10 @@
+"use client";
 import { routes } from "@/config/routes";
-import { ClassifiedWithImages, MultiStepForEnum } from "@/config/types";
+import {
+  ClassifiedWithImages,
+  favourites,
+  MultiStepForEnum,
+} from "@/config/types";
 import {
   Classified,
   Colour,
@@ -13,9 +18,14 @@ import Link from "next/link";
 import { HtmlParser } from "../shared/html-parser";
 import { Cog, Fuel, GaugeCircle, Paintbrush2 } from "lucide-react";
 import { Button } from "../ui/button";
+import { FavouriteButton } from "./favourite-button";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface ClassifiedCardProps {
   classified: ClassifiedWithImages;
+  favourites: number[];
 }
 
 function formatNumber(num: number | null, options?: Intl.NumberFormatOptions) {
@@ -109,75 +119,100 @@ const getKeyClassifiedInfo = (classified: ClassifiedWithImages) => {
 };
 
 export const ClassifiedCard = (props: ClassifiedCardProps) => {
-  const { classified } = props;
+  const { classified, favourites } = props;
+  const pathname = usePathname();
+  const [isFavourite, setIsFavourite] = useState(
+    favourites.includes(classified.id)
+  );
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (!isFavourite && pathname === routes.favourites) setIsVisible(false);
+  }, [isFavourite, pathname]);
+
   return (
-    <div className="bg-white relative rounded-md shadow-md overflow-hidden flex flex-col">
-      <div className="aspect-3/2 relative">
-        <Link href={routes.singleClassified(classified.slug)}>
-          <Image
-            placeholder="blur"
-            blurDataURL={classified.images[0].blurhash}
-            src={classified.images[0].src}
-            alt={classified.images[0].alt}
-            className="object-cover"
-            fill={true}
-            quality={25}
-          />
-        </Link>
-        <div className="absolute top-2.5 right-3.5 bg-primary text-slate-50 font-bolt px-2 py-1 rounded">
-          <p className="text-xs lg:text-base xl:text-lg font-semibold">
-            {classified.price}
-          </p>
-        </div>
-      </div>
-      <div className="p-4 flex flex-col space-y-3">
-        <Link
-          href={routes.singleClassified(classified.slug)}
-          className="text-black text-sm md:text-base lg:text-lg font-semibold line-clamp-1 transition-colors hover:text-primary"
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          initial={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className="bg-white relative rounded-md shadow-md overflow-hidden flex flex-col"
         >
-          {classified.title}
-        </Link>
-        {classified?.description && (
-          <div className="text-xs md:text-sm xl:text-base text-gray-500 line-clamp-2">
-            <HtmlParser html={classified.description} />
-            &nbsp; {/* Used for equal spacing across each card in the grid */}
+          <div className="aspect-3/2 relative">
+            <Link href={routes.singleClassified(classified.slug)}>
+              <Image
+                placeholder="blur"
+                blurDataURL={classified.images[0].blurhash}
+                src={classified.images[0].src}
+                alt={classified.images[0].alt}
+                className="object-cover"
+                fill={true}
+                quality={25}
+              />
+            </Link>
+            <FavouriteButton isFavourite={isFavourite} setIsFavourite={setIsFavourite} id={classified.id}/>
+            <div className="absolute top-2.5 right-3.5 bg-primary text-slate-50 font-bolt px-2 py-1 rounded">
+              <p className="text-xs lg:text-base xl:text-lg font-semibold">
+                {classified.price}
+              </p>
+            </div>
           </div>
-        )}
+          <div className="p-4 flex flex-col space-y-3">
+            <Link
+              href={routes.singleClassified(classified.slug)}
+              className="text-black text-sm md:text-base lg:text-lg font-semibold line-clamp-1 transition-colors hover:text-primary"
+            >
+              {classified.title}
+            </Link>
+            {classified?.description && (
+              <div className="text-xs md:text-sm xl:text-base text-gray-500 line-clamp-2">
+                <HtmlParser html={classified.description} />
+                &nbsp;{" "}
+                {/* Used for equal spacing across each card in the grid */}
+              </div>
+            )}
 
-        <ul className="text-xs md:text-xs text-gray-600 xl:flex grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-4 items-center justify-between w-full">
-          {getKeyClassifiedInfo(classified)
-            .filter((v) => v.value)
-            .map(({ id, icon, value }) => (
-              <li
-                key={id}
-                className="font-bold flex xl:flex-col items-center gap-x-1.5"
+            <ul className="text-xs md:text-xs text-gray-600 xl:flex grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-4 items-center justify-between w-full">
+              {getKeyClassifiedInfo(classified)
+                .filter((v) => v.value)
+                .map(({ id, icon, value }) => (
+                  <li
+                    key={id}
+                    className="font-bold flex xl:flex-col items-center gap-x-1.5"
+                  >
+                    {icon}
+                    {value}
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <div className="p-2 mt-4 flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:gap-x-2 w-full ">
+            <Button
+              className="flex-1 text-black transition-colors hover:border-white hover:bg-primary hover:text-white py-2 lg:py-2.5 h-full text-xs md:text-sm xl:text-base"
+              asChild
+              variant="outline"
+              size="sm"
+            >
+              <Link
+                href={routes.reserve(classified.slug, MultiStepForEnum.WELCOME)}
               >
-                {icon}
-                {value}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className="p-2 mt-4 flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:gap-x-2 w-full ">
-        <Button
-          className="flex-1 text-black transition-colors hover:border-white hover:bg-primary hover:text-white py-2 lg:py-2.5 h-full text-xs md:text-sm xl:text-base"
-          asChild
-          variant="outline"
-          size="sm"
-        >
-          <Link href={routes.reserve(classified.slug, MultiStepForEnum.WELCOME)}>Reserve</Link>
-        </Button>
+                Reserve
+              </Link>
+            </Button>
 
-        <Button
-          className="flex-1 py-2 lg:py-2.5 h-full text-xs md:text-sm xl:text-base"
-          asChild
-          size="sm"
-        >
-          <Link href={routes.singleClassified(classified.slug)}>
-            View Details
-          </Link>
-        </Button>
-      </div>
-    </div>
+            <Button
+              className="flex-1 py-2 lg:py-2.5 h-full text-xs md:text-sm xl:text-base"
+              asChild
+              size="sm"
+            >
+              <Link href={routes.singleClassified(classified.slug)}>
+                View Details
+              </Link>
+            </Button>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
--- src/components/inventory/classified-list.tsx
+++ src/components/inventory/classified-list.tsx
@@ -1,18 +1,19 @@
-import { ClassifiedWithImages } from "@/config/types";
+import { ClassifiedWithImages, favourites } from "@/config/types";
 import { Classified } from "@prisma/client";
 import { ClassifiedCard } from "./classified-card";
 
 interface ClassifiedListProps {
   classifieds: ClassifiedWithImages[];
+  favourites: number[];
 }
 
 export const ClassifiedList = (props: ClassifiedListProps) => {
-  const { classifieds } = props;
+  const { classifieds, favourites } = props;
 
   return (
     <div className="grid grid=cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4">
       {classifieds.map((classified) => (
-        <ClassifiedCard key={classified.id} classified={classified} />
+        <ClassifiedCard key={classified.id} classified={classified} favourites={favourites} />
       ))}
     </div>
   );
