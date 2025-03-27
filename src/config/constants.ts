f4252c1474463cdb1e31ba38a96b6d5812f02973
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-03-27 14:47:39 +0530

    add CLASSIFIEDS_PER_PAGE constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,3 +2,5 @@ export const imageSources = {
   classifiedPlaceholder:
     "https://majestic-motors.s3.eu-west-2.amazonaws.com/uploads/classified-placeholder.jpeg",
 };
+
+export const CLASSIFIEDS_PER_PAGE = 5;
