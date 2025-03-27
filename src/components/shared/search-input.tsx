be2470c1310d6aad34a71f49ca96d3dcc811ccf7
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-03-28 00:19:45 +0530

    add search-input in sidebar

---
+++ src/components/shared/search-input.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useQueryState } from "nuqs";
+import { type ChangeEvent, useCallback, useRef } from "react";
+import debounce from "debounce";
+import { SearchIcon, XIcon } from "lucide-react";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+
+function debounceFunc<T extends (...args: any[]) => any>(
+  fn: T,
+  wait: number,
+  opts: { immediate: boolean }
+) {
+  return debounce(fn, wait, opts);
+}
+
+interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export const SearchInput = (props: SearchInputProps) => {
+  const { className, ...rest } = props;
+
+  const [q, setSearch] = useQueryState("q", { shallow: false });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearch = useCallback(
+    debounceFunc(
+      (value: string) => {
+        setSearch(value);
+      },
+      1000,
+      { immediate: true }
+    ),
+    []
+  );
+
+  const onchange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    handleSearch(value);
+  };
+
+  const clearSearch = () => {
+    setSearch(null);
+    if (inputRef.current) inputRef.current.value = "";
+  }
+
+  return (
+    <form className="relative flex-1 text-black">
+      <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500" />
+      <Input
+        ref={inputRef}
+        defaultValue={q || ""}
+        className={cn(className, "pl-8")}
+        onChange={onchange}
+        type="text"
+        {...rest}
+      />
+      {q && (
+        <XIcon
+          className="absolute right-2.5 top-2.5 h-4 w-4 text-white bg-gray-500 p-0.5 rounded-full cursor-pointer"
+          onClick={clearSearch}
+        />
+      )}
+    </form>
+  );
+};
