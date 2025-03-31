99c881121dcaed3b86ea6304c429902360963c67
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-03-31 20:26:48 +0530

    added slug pages

---
+++ src/app/(presentation)/inventory/[slug]/(classified)/not-available/page.tsx
@@ -0,0 +1,21 @@
+
+import { EndButtons } from "@/components/shared/ends-button";
+import { XCircle } from "lucide-react";
+
+export default function NotAvailablePage() {
+	return (
+		<div className="flex items-center justify-center min-h-[80dvh] text-black">
+			<div className="flex flex-col items-center p-8 space-y-4">
+				<XCircle className="w-16 h-16 text-muted-foreground" />
+				<p className="text-lg font-semibold text-center">
+					Sorry, that vehicle is no longer available.
+				</p>
+				<p className="text-center text-muted-foreground">
+					We have a large number of other vehicles that might suit your needs,
+					to view our current stock please check our website.
+				</p>
+				<EndButtons />
+			</div>
+		</div>
+	);
+}
