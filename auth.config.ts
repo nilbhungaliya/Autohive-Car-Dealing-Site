6eca8b0958088968cf6c5ae5dbf8cccf766ae425
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-03 22:41:53 +0530

    add auth.config.ts for authentication

---
+++ auth.config.ts
@@ -0,0 +1,91 @@
+import type { NextAuthConfig, User } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "@/lib/db";
+import { SESSION_MAX_AGE } from "@/config/constants";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { SignInSchema } from "@/app/schemas/auth.schema";
+import { bcryptPasswordCompare } from "@/lib/bcrypt";
+import ResendProvider from "next-auth/providers/resend";
+import { routes } from "@/config/routes";
+import type { AdapterUser } from "@auth/core/adapters";
+
+export const config = {
+  adapter: PrismaAdapter(db),
+  useSecureCookies: false,
+  trustHost: true,
+  session: {
+    strategy: "database",
+    maxAge: SESSION_MAX_AGE / 1000,
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials): Promise<User | null> => {
+        try {
+          const validatedField = SignInSchema.safeParse(credentials);
+
+          if (!validatedField.success) return null;
+
+          const user = await db.user.findUnique({
+            where: { email: validatedField.data.email },
+            select: { id: true, email: true, hashedPassword: true },
+          });
+
+          if (!user) return null;
+
+          const match = await bcryptPasswordCompare(
+            validatedField.data.password,
+            user.hashedPassword
+          );
+
+          if (!match) return null;
+
+          return { ...user, requires2FA: true };
+        } catch (error) {
+          console.log({ error });
+          return null;
+        }
+      },
+    }),
+    ResendProvider({}),
+  ],
+  pages: {
+    signIn: routes.signIn,
+    signOut: "/auth/sign-out",
+    error: "/auth/error",
+  },
+  callbacks: {
+    async jwt({ user, token }) {
+      const session = await db.session.create({
+        data: {
+          sessionToken: crypto.randomUUID(),
+          userId: user.id as string,
+          expires: new Date(Date.now() + SESSION_MAX_AGE),
+          requires2FA: user.requires2FA as boolean,
+        },
+      });
+      // console.log({ session }, "session");
+      if (!session) return null;
+
+      if (user) token.requires2FA = user.requires2FA;
+
+      token.id = session.sessionToken;
+      token.exp = session.expires.getTime();
+
+      return token;
+    },
+    async session({ session, user }) {
+      session.user = {
+        id: session.userId,
+        email: user.email,
+      } as AdapterUser;
+      return session;
+    },
+  },
+  jwt: {
+		encode: async ({ token }) => token?.id as string,
+	},
+} as NextAuthConfig;
