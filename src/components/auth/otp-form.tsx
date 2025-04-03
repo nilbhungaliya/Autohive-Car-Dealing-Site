cdc64fad193c90de611cce50201feddeba8c2f0e
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-03 22:48:58 +0530

    add otp-form and otp-input in auth component

---
+++ src/components/auth/otp-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { OneTimePasswordSchema, type OtpSchemaType } from "@/app/schemas/otp.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "../ui/form";
+import { Loader2, RotateCw } from "lucide-react";
+import { Button } from "../ui/button";
+import { OneTimePasswordInput } from "./otp-input";
+
+export const OtpForm = () => {
+    const [isCodePending, startCodeTransition] = useTransition();
+    const [isSubmitPending, startSubmitTransition] = useTransition();
+    const router = useRouter();
+
+    const form = useForm<OtpSchemaType>({
+        resolver: zodResolver(OneTimePasswordSchema),
+    })
+
+    const onSubmit : SubmitHandler<OtpSchemaType> = (data) => {
+        startSubmitTransition(async () => {
+            
+        })
+    }
+
+    const [sendButtonText, setSendButtonText] = useState("Send code");
+
+    const sendCode = async()=>{}
+
+    return (
+		<div className="min-h-[calc(100vh-4rem)] flex w-full flex-1 justify-center px-6 pt-10 lg:items-center lg:pt-0">
+			<div className="flex w-full max-w-lg flex-col">
+				<h3 className="mb-4 text-4xl lg:text-5xl text-center">
+					One Time Password
+				</h3>
+				<p className="mb-12 text-center text-slate-500">
+					Enter the six digit code sent to your email
+				</p>
+
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							control={form.control}
+							name="code"
+							render={({ field: { value, onChange, ...rest } }) => (
+								<FormItem className="mb-8">
+									<FormControl>
+										<OneTimePasswordInput
+											type="number"
+											setValue={onChange}
+											{...rest}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<div className="flex w-full items-center justify-center">
+							<button
+								type="button"
+								className="flex items-centewr gap-2.5 text-base font-medium text-slate-600 transition-colors duration-200 hover:text-primary group cursor-pointer"
+								onClick={sendCode}
+								disabled={isCodePending}
+							>
+								{isCodePending ? (
+									<Loader2 className="w-6 h-6 text-secondary transition-colors duration-200 group-hover:text-primary animate-spin" />
+								) : (
+									<RotateCw className="w-6 h-6 text-secondary transition-colors duration-200 group-hover:text-primary" />
+								)}
+								{sendButtonText}
+							</button>
+						</div>
+						<div className="mt-6 flex w-full flex-col gap-4 md:mt-16">
+							<Button
+								className="flex w-full gap-x-2"
+								disabled={isSubmitPending}
+							>
+								<span className="text-sm uppercase tracking-wider text-inherit">
+									{isSubmitPending ? "Verifying..." : "Verify"}
+								</span>
+								{isSubmitPending ? (
+									<Loader2 className="w-4 h-4 shrink-0 animate-spin" />
+								) : null}
+							</Button>
+						</div>
+					</form>
+				</Form>
+			</div>
+		</div>
+	);
+};
---
+++ src/components/auth/otp-input.tsx
@@ -0,0 +1,153 @@
+import { cn } from "@/lib/utils";
+import type React from "react";
+import { forwardRef, useRef } from "react";
+
+interface PinCodeProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "size" | "type" | "value"
+  > {
+  setValue: React.Dispatch<React.SetStateAction<string | number | undefined>>;
+  type?: "text" | "number";
+  length?: number;
+  center?: boolean;
+  inputClassName?: string;
+}
+
+const containerClassNames = {
+  base: "flex flex-row",
+  center: "justify-center align-center",
+};
+
+const inputClassNames = {
+  base: "block peer text-center texct-slate-950 bg-transparent mr-2 focus:placeholder:opacity-0 focus:outline-hidden transition duration-200 disabled:bg-zinc-50 disabled:placeholder:text-gray-400 disabled:text-gray-400 disabled:cursor-not-allowed disabled:border-gray-200 rounded focus:outline-hidden focus:border-primary",
+  size: "p-2 text-2xl font-medium w-12 h-12 sm:h-[70px] sm: w-[70px]",
+  color: {
+    base: "bg-transparent focus:ring-[0.6px] border border-gray-300 focus-visible:border-secondary focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-secondary placeholder:text-slate-950",
+    active: "not-read-only:hover:enabled:border-secondary focus:ring-secondary",
+  },
+};
+
+export const OneTimePasswordInput = forwardRef<HTMLInputElement, PinCodeProps>(
+  (props, ref) => {
+    const {
+      type,
+      length = 6,
+      center = true,
+      defaultValue,
+      inputClassName,
+      setValue,
+      className,
+      ...rest
+    } = props;
+    const inputRefs = useRef<HTMLInputElement[]>([]);
+
+    function addInputRefs(index: number) {
+      return (element: HTMLInputElement) => {
+        if (element) {
+          inputRefs.current[index] = element;
+        }
+      };
+    }
+
+    function setPinValue() {
+      setValue(inputRefs.current.map((input) => input.value).join(""));
+    }
+
+    function handleChange(
+      event: React.ChangeEvent<HTMLInputElement>,
+      index: number
+    ) {
+      const inputValues = event.target.value.split("");
+      inputRefs.current[index].value = inputValues[inputValues.length - 1];
+      if (index < length - 1) inputRefs.current[index + 1].focus();
+      setPinValue();
+    }
+
+    function handlePaste(
+      event: React.ClipboardEvent<HTMLInputElement>,
+      index: number
+    ) {
+      const copiedValues = event.clipboardData.getData("text").trim().split("");
+      const isComplete = copiedValues.length >= length;
+
+      for (let i = 0; i < length - index; i++) {
+        inputRefs.current[index + i].value = copiedValues[i] ?? "";
+        if (index + i === length - 1) {
+          inputRefs.current[index + i].focus();
+        } else {
+          inputRefs.current[index + i + 1].focus();
+        }
+      }
+      event.preventDefault();
+      setPinValue();
+
+      if (isComplete) {
+        const form = event.currentTarget.closest("form");
+        form?.requestSubmit();
+      }
+    }
+
+    function handleKeyDown(event: React.KeyboardEvent, index: number) {
+      const currentValue = inputRefs.current[index].value;
+
+      if (event.key === "ArrowRight" && index < length - 1) {
+        inputRefs.current[index + 1].focus(); // Step 1
+      }
+
+      if (event.key === "ArrowLeft" && index > 0) {
+        inputRefs.current[index - 1].focus(); // Step 2
+      }
+
+      if (event.key === "BackSpace") {
+        if (currentValue !== "") {
+          inputRefs.current[index].value = ""; // Step 3
+        } else {
+          if (index === 0) {
+            return; // Step 4
+          }
+          inputRefs.current[index - 1].value = ""; // Step 5
+          inputRefs.current[index - 1].focus(); // Step 6
+        }
+        setPinValue(); // Step 7
+      }
+    }
+
+    return (
+      <div className="flex flex-col" ref={ref}>
+        <div
+          className={cn(
+            className,
+            center && containerClassNames.center,
+            containerClassNames.base
+          )}
+        >
+          {Array.from({ length }, (_, i) => i).map((i) => {
+            return (
+              <input
+                key={`otp-input-${i}`}
+                ref={addInputRefs(i)}
+                type={type}
+                inputMode={type === "text" ? type : "numeric"}
+                autoCapitalize="off"
+                autoCorrect="off"
+                autoComplete="off"
+                onChange={(event) => handleChange(event, i)}
+                onPaste={(event) => handlePaste(event, i)}
+                onKeyDown={(event) => handleKeyDown(event, i)}
+                className={cn(
+                  inputClassNames.base,
+                  inputClassNames.size,
+                  inputClassNames.color.active,
+                  inputClassNames.color.base,
+                  inputClassName,
+                  "[&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                )}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+);
