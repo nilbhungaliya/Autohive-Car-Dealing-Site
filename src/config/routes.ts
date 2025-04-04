d0ec3b05491dc0e64b0fa06749842e02ea087985
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-04 19:22:52 +0530

    Add admin dashboard route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,13 +1,17 @@
 import { MultiStepFormEnum } from "./types";
 
 export const routes = {
-    home : "/",
-    singleClassified : (slug: string) => `/inventory/${slug}`,
-    reserve : (slug: string, step: MultiStepFormEnum) => `/inventory/${slug}/reserve?step=${step}`,
-    favourites : "/favourites",
-    inventory : "/inventory",
-    notAvailable : (slug: string) => `/inventory/${slug}/not-available`,
-    success: (slug: string) => `/inventory/${slug}/success`,
-    signIn : "/auth/sign-in",
-    challenge : "/auth/challenge",
-}
+  home: "/",
+  singleClassified: (slug: string) => `/inventory/${slug}`,
+  reserve: (slug: string, step: MultiStepFormEnum) =>
+    `/inventory/${slug}/reserve?step=${step}`,
+  favourites: "/favourites",
+  inventory: "/inventory",
+  notAvailable: (slug: string) => `/inventory/${slug}/not-available`,
+  success: (slug: string) => `/inventory/${slug}/success`,
+  signIn: "/auth/sign-in",
+  challenge: "/auth/challenge",
+  admin: {
+    dashboard: "/admin/dashboard",
+  },
+};
