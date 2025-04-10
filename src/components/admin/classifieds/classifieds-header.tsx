7f49d86a882b0d723523023af232bda419232ca1
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-10 16:11:40 +0530

    improve classified-header component

--- src/components/admin/classifieds/classifieds-header.tsx
+++ src/components/admin/classifieds/classifieds-header.tsx
@@ -4,18 +4,18 @@ import { ClassifiedStatus } from "@prisma/client";
 import { CreateClassifiedDialog } from "./create-classified-dialog";
 
 export const AdminClassifiedsHeader = ({ searchParams }: AwaitedPageProps) => {
-	return (
-		<div className="flex flex-col p-6 space-y-4 text-muted">
-			<div className="flex items-center justify-between">
-				<h1 className="font-semibold text-lg md:text-2xl">All Classifieds</h1>
-				<div className="flex items-center justify-between">
-					<RadioFilter
-						items={["ALL", ...Object.values(ClassifiedStatus)]}
-						searchParams={searchParams}
-					/>
-					<CreateClassifiedDialog />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col p-6 space-y-4 text-muted">
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold text-lg md:text-2xl">All Classifieds</h1>
+        <div className="flex items-center justify-between">
+          <RadioFilter
+            items={["ALL", ...Object.values(ClassifiedStatus)]}
+            searchParams={searchParams}
+          />
+          <CreateClassifiedDialog />
+        </div>
+      </div>
+    </div>
+  );
 };
