3f062db4eb15c5ac5977f320b580fa58ebdc5cbf
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-11 20:05:50 +0530

    Add edit page

---
+++ src/app/admin/classifieds/edit/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { validateIdSchema } from "@/app/schemas/id.schema";
+import { ClassifiedForm } from "@/components/admin/classifieds/classified-form";
+import { routes } from "@/config/routes";
+import { PageProps } from "@/config/types";
+import db from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export default async function EditClassified(props:PageProps) {
+    const params = await props.params;
+
+    const { data, success } = validateIdSchema.safeParse({
+		id: Number(params?.id),
+	});
+
+    if (!success) redirect(routes.admin.classifieds);
+    const classified = await db.classified.findUnique({
+        where:{id:data.id},
+        include:{images:true}
+    })
+    if(!classified) redirect(routes.admin.classifieds);
+
+
+    return <ClassifiedForm classified={classified} />
+}
