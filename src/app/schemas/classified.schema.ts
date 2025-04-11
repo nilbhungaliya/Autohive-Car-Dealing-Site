974362a56e7b725f4b88b1022b6c3c7eb65835b5
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-11 20:04:24 +0530

    Add updateClassifiedSchema

--- src/app/schemas/classified.schema.ts
+++ src/app/schemas/classified.schema.ts
@@ -1,3 +1,4 @@
+import { BodyType, ClassifiedStatus, Colour, CurrencyCode, FuelType, OdoUnit, Transmission, ULEZCompliance } from "@prisma/client";
 import { z } from "zod";
 
 export const ClassifiedFilterSchema = z.object({
@@ -21,3 +22,39 @@ export const ClassifiedFilterSchema = z.object({
     seats: z.string().optional(),
     ulezCompliance: z.string().optional(),
   });
+
+  export const updateClassifiedSchema = z.object({
+    id: z.number(),
+    year: z.string(),
+    make: z.string(),
+    model: z.string(),
+    modelVariant: z.string().optional(),
+    description: z.string(),
+    vrm: z.string(),
+    odoReading: z.number(),
+    doors: z.number().min(1).max(8),
+    seats: z.number().min(1).max(12),
+    ulezCompliance: z.nativeEnum(ULEZCompliance, {
+      message: "Invalid ULEZ Compliance",
+    }),
+    transmission: z.nativeEnum(Transmission, { message: "Invalid Transmission" }),
+    colour: z.nativeEnum(Colour, { message: "Invalid Colour" }),
+    fuelType: z.nativeEnum(FuelType, { message: "Invalid Fuel Type" }),
+    bodyType: z.nativeEnum(BodyType, { message: "Invalid Body Type" }),
+    odoUnit: z.nativeEnum(OdoUnit, { message: "Invalid Odo Unit" }),
+    status: z.nativeEnum(ClassifiedStatus),
+    currency: z.nativeEnum(CurrencyCode, { message: "Invalid Currency Code" }),
+    price: z.number(),
+    images: z.array(
+      z.object({
+        id: z.number().optional(),
+        src: z.string().url(),
+        alt: z.string(),
+        uuid: z.string().uuid().optional(),
+        base64: z.string().optional(),
+        done: z.boolean().optional(),
+      }),
+    ),
+  });
+  
+  export type UpdateClassifiedType = z.infer<typeof updateClassifiedSchema>;
