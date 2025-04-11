6a0557cf9a486c20f8a52b8746186b1d6f0b968c
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-11 20:20:24 +0530

    Add some config in next-config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,52 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compress:true,
+  reactStrictMode: true,
+  compiler: {
+		removeConsole: process.env.NODE_ENV === "production",
+	},
+  experimental:{
+    authInterrupts: true,
+    nodeMiddleware: true,
+    reactCompiler: true,
+    optimizeCss: true,
+    optimizeServerReact: true,
+    optimizePackageImports: ["lucide-react", "data-fns"],
+  },
   /* config options here */
   images:{
     remotePatterns:[{hostname: "*"}]
-  }
+  },
+  async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=63072000; includeSubDomains; preload",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "DENY",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "Permissions-Policy",
+						value: `camera=(), microphone=(), geolocation=(), midi=(), sync-xhr=(), fullscreen=(self "${process.env.NEXT_PUBLIC_APP_URL}"), geolocation=("${process.env.NEXT_PUBLIC_APP_URL}")`,
+					},
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
