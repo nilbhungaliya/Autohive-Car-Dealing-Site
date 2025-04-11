4a1af08bb7f8a647b965052d15099ed516a4ddb6
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-11 20:12:41 +0530

    Add classified-form-fieds

---
+++ src/components/admin/classifieds/classified-form-fieds.tsx
@@ -0,0 +1,274 @@
+"use client";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { NumberInput } from "@/components/ui/number-input";
+import { SelectAdmin } from "@/components/ui/selectAdmin";
+import {
+  formatBodyType,
+  formatColour,
+  formatFuelType,
+  formatTransmission,
+  formatUlezCompliance,
+  generateYears,
+} from "@/lib/utils";
+import {
+  BodyType,
+  Colour,
+  CurrencyCode,
+  FuelType,
+  OdoUnit,
+  Transmission,
+  ULEZCompliance,
+} from "@prisma/client";
+import { useFormContext } from "react-hook-form";
+import { TaxonomySelects } from "./taxonomy-selects";
+import { InputSelect } from "@/components/ui/input-select";
+
+const years = generateYears(1925);
+
+export const ClassifiedFormFields = () => {
+  const form = useFormContext();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-muted">
+      <FormField
+        control={form.control}
+        name="year"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="year">Year</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={years.map((year) => ({
+                  label: year,
+                  value: year,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <TaxonomySelects />
+      <InputSelect
+        options={Object.values(CurrencyCode).map((value) => ({
+          label: value,
+          value,
+        }))}
+        label="Price"
+        inputName="price"
+        selectName="currency"
+        inputMode="numeric"
+        placeholder="0"
+        className="h-10"
+      />
+
+      <InputSelect
+        options={Object.values(OdoUnit).map((value) => ({
+          label: value,
+          value,
+        }))}
+        label="Odometer Reading"
+        inputName="odoReading"
+        selectName="odoUnit"
+        inputMode="numeric"
+        placeholder="0"
+        className="h-10"
+      />
+
+      <FormField
+        control={form.control}
+        name="transmission"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="transmission">Transmission</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={Object.values(Transmission).map((value) => ({
+                  label: formatTransmission(value),
+                  value,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="fuelType"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="fuelType">Fuel Type</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={Object.values(FuelType).map((value) => ({
+                  label: formatFuelType(value),
+                  value,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="bodyType"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="bodyType">Body Type</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={Object.values(BodyType).map((value) => ({
+                  label: formatBodyType(value),
+                  value,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="colour"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="colour">Colour</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={Object.values(Colour).map((value) => ({
+                  label: formatColour(value),
+                  value,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="ulezCompliance"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="ulezCompliance">ULEZ Compliance</FormLabel>
+            <FormControl>
+              <SelectAdmin
+                selectClassName="text-muted/75 bg-primary-800 border-transparent"
+                options={Object.values(ULEZCompliance).map((value) => ({
+                  label: formatUlezCompliance(value),
+                  value,
+                }))}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="vrm"
+        render={({ field: { ref, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="vrm">Vehicle Registration Mark</FormLabel>
+            <FormControl>
+              <Input
+                placeholder="LA16 PYW"
+                className="uppercase text-muted h-10 mt-1 placeholder:text-muted/75"
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="doors"
+        render={({ field: { onChange, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="doors">Doors</FormLabel>
+            <FormControl>
+              <NumberInput
+                max={6}
+                min={1}
+                placeholder="0"
+                // style={{ background: "#081a2b" }}
+                className="text-muted placeholder:text-muted/75"
+                onValueChange={(values) => {
+                  onChange(values.floatValue);
+                }}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="seats"
+        render={({ field: { onChange, ...rest } }) => (
+          <FormItem>
+            <FormLabel htmlFor="seats">Seats</FormLabel>
+            <FormControl>
+              <NumberInput
+                max={8}
+                min={1}
+                placeholder="0"
+                // style={{ background: "#081a2b" }}
+                className="text-muted placeholder:text-muted/75"
+                onValueChange={(values) => {
+                  onChange(values.floatValue);
+                }}
+                {...rest}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <div className="col-span-2">
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field: { onChange, ...rest } }) => (
+            <FormItem>
+              <FormControl>
+                {/* <RichTextEditor
+									label="Description"
+									config={{
+										init: { placeholder: "Enter your vehicle's description" },
+									}}
+									{...rest}
+								/> */}
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
