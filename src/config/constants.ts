9b156849d3c486d0ed912a18311521a5553079f5
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-04-14 21:56:01 +0530

    Add sortOrder, classifiedBadgeMap and customerBadgeMap constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,7 @@
 import { env } from "@/env";
 import { routes } from "./routes";
+import { ClassifiedStatus, CustomerStatus } from "@prisma/client";
+import type { BadgeProps } from "@/components/ui/badge";
 
 export const imageSources = {
   classifiedPlaceholder:"https://autohive-motors.imgix.net/uploads/AdobeStock_855683950.jpeg",
@@ -18,3 +20,22 @@ export const SESSION_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
 
 export const MAX_IMAGE_SIZE = 20 * 1000 * 1000; // 2mb
 export const MAX_IMAGES = 20;
+
+export const sortOrder = ["asc", "desc"] as const;
+
+export const ClassifiedBadgeMap: Record<
+	ClassifiedStatus,
+	BadgeProps["variant"]
+> = {
+	[ClassifiedStatus.DRAFT]: "secondary",
+	[ClassifiedStatus.LIVE]: "default",
+	[ClassifiedStatus.SOLD]: "destructive",
+};
+
+export const CustomerBadgeMap: Record<CustomerStatus, BadgeProps["variant"]> = {
+	[CustomerStatus.COLD]: "secondary",
+	[CustomerStatus.CONTACTED]: "default",
+	[CustomerStatus.INTERESTED]: "destructive",
+	[CustomerStatus.PURCHASED]: "warning",
+	[CustomerStatus.SUBSCRIBER]: "info",
+};
