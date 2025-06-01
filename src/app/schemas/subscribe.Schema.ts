1d835752b7135d761ac01c1c99718ae584221919
Nil Bhungaliya <nilbhungaliya1204@gmail.com>
2025-06-01 19:36:25 +0530

    fix the subscribe schema issue

--- src/app/schemas/subscribe.Schema.ts
+++ src/app/schemas/subscribe.Schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const SubscribeSchema = z.object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
-    email: z.string().email({ message: "Invalid email address" }),
+    firstName: z.string().min(2, "First name must be at least 2 characters"),
+    lastName: z.string().min(2, "Last name must be at least 2 characters"),
+    email: z.string().email("Please enter a valid email address"),
   });
